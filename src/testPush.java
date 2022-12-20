e4fe9b136a3e5672489de97db7100fe8c84c1322
Nhan Nguyen <nguyenlethunhan@gmail.com>
2022-12-20 14:08:12 +0700

    Create a text file

--- src/testPush.java
+++ src/testPush.java
@@ -1,5 +1,12 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class testPush {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Testing the git push");
+        PrintWriter pw = new PrintWriter(new FileWriter("src/test.txt", false));
+        pw.println("This is the text file");
+        pw.close();
     }
 }
